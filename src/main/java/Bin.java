9254589a95bf4fb9bd0f93fd18d8506528855e09
wafflescloud <e1354308@u.nus.edu>
2025-08-15 17:04:21 +0800

    Greeting & exit

--- src/main/java/Bin.java
+++ src/main/java/Bin.java
@@ -1,14 +1,23 @@
-public class Duke {
+public class Bin {
     private static final String greeting = "Hello! I'm Bin\nWhat can I do for you?\n";
     private static final String line = " ___________________________________________\n";
+    private static final String logo = "  _____    _  \n"
+                                    + " |  __  \\ |_|\n"
+                                    + " | |__| / ___  ________\n"
+                                    + " |  __  \\|   ||   __   |\n"
+                                    + " | |__| ||   ||  |  |  |\n"
+                                    + " |_____/ |___||__|  |__|\n"
+                                    + "\n";
+    private static final String exit = "Bye. Hope to see you again soon!\n";
+    public static String greeting() {
+        return line + logo + greeting;
+    }
+
+    public static String exit() {
+        return line + exit + line;
+    }
     public static void main(String[] args) {
-        String logo = "  _____    _  \n"
-                    + " |  __  \\ |_|\n"
-                    + " | |__| / ___  ________\n"
-                    + " |  __  \\|   ||   __   |\n"
-                    + " | |__| ||   ||  |  |  |\n"
-                    + " |_____/ |___||__|  |__|\n"
-                    + "\n";
-        System.out.println(line + logo + greeting + line);
+
+        System.out.println(greeting() + exit());
     }
 }
