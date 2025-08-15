23418bf978cd828a8d85f29c8c895bc76a1a3319
wafflescloud <e1354308@u.nus.edu>
2025-08-15 17:30:56 +0800

    Add support for echo

--- src/main/java/Bin.java
+++ src/main/java/Bin.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Bin {
-    private static final String greeting = "Hello! I'm Bin\nWhat can I do for you?\n";
-    private static final String line = " ___________________________________________\n";
+    private static final String greeting = "    Hello! I'm Bin\n" +
+                                           "    What can I do for you?\n";
+    private static final String line = "  ___________________________________________\n";
     private static final String logo = "  _____    _  \n"
                                     + " |  __  \\ |_|\n"
                                     + " | |__| / ___  ________\n"
@@ -8,16 +11,22 @@ public class Bin {
                                     + " | |__| ||   ||  |  |  |\n"
                                     + " |_____/ |___||__|  |__|\n"
                                     + "\n";
-    private static final String exit = "Bye. Hope to see you again soon!\n";
+    private static final String exit = "    Bye. Hope to see you again soon!\n";
     public static String greeting() {
-        return line + logo + greeting;
+        return line + logo + greeting + line;
     }
 
     public static String exit() {
         return line + exit + line;
     }
     public static void main(String[] args) {
-
-        System.out.println(greeting() + exit());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(greeting());
+        String action = scanner.nextLine();
+        while (!action.equals("bye")) {
+            System.out.println(line + "\n   " + action + "\n" + line);
+            action = scanner.nextLine();
+        }
+        System.out.println(exit());
     }
 }
